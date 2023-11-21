97647e5cadd3a9c8b356e728d6218da10edb4a4a
TheAzylium <34239874+TheAzylium@users.noreply.github.com>
2023-11-21 22:34:38 +0100

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,97 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
+import { LeafletModule } from '@asymmetrik/ngx-leaflet';
+import {
+  icon,
+  Icon,
+  latLng,
+  Layer,
+  LayerGroup,
+  LeafletEvent,
+  Map,
+  marker,
+  tileLayer,
+} from 'leaflet';
+import { LANTERN } from './markers/lantern';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet],
+  imports: [CommonModule, RouterOutlet, LeafletModule],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.scss',
 })
 export class AppComponent {
-  title = 'lanterne';
+  // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+  // @ts-expect-error
+  map: Map;
+
+  AtlasMap = tileLayer('/assets/mapStyles/styleAtlas/{z}/{x}/{y}.jpg', {
+    minZoom: 0,
+    maxZoom: 5,
+    noWrap: true,
+    id: 'AtlasStyle map',
+  });
+  GridMap = tileLayer('/assets/mapStyles/styleGrid/{z}/{x}/{y}.png', {
+    minZoom: 0,
+    maxZoom: 5,
+    noWrap: true,
+    id: 'GridStyle map',
+  });
+  SatelliteMap = tileLayer('/assets/mapStyles/styleSatelite/{z}/{x}/{y}.jpg', {
+    minZoom: 0,
+    maxZoom: 5,
+    noWrap: true,
+    id: 'SatelliteStyle map',
+  });
+  lantern: LayerGroup = new LayerGroup();
+
+  options = {
+    layers: [this.AtlasMap],
+    zoom: 3,
+    maxZoom: 5,
+    minZoom: 0,
+    center: latLng(0, 0),
+    noWrap: true,
+  };
+
+  layersControl = {
+    baseLayers: {
+      AtlasMap: this.AtlasMap,
+      GridMap: this.GridMap,
+      SatelliteMap: this.SatelliteMap,
+    },
+    overlays: {},
+  };
+
+  clickEvent(event: LeafletEvent): void {
+    console.log(event);
+  }
+
+  onMapReady(map: Map): void {
+    this.map = map;
+    this.setLantern();
+  }
+
+  setLantern(): void {
+    for (const mark of LANTERN) {
+      const newMarker = marker(latLng(mark.lat, mark.lng), {
+        title: mark.name,
+        icon: icon({
+          ...Icon.Default.prototype.options,
+          iconUrl: 'assets/marker/lantern.png',
+          shadowUrl: 'assets/marker-shadow.png',
+        }),
+      });
+      this.lantern.addLayer(newMarker);
+    }
+
+    this.layersControl.overlays = {
+      ...this.layersControl.overlays,
+      Lanterne: this.lantern,
+    };
+
+    this.map.addLayer(this.lantern);
+  }
 }
---
+++ src/app/markers/lantern.ts
@@ -0,0 +1,11 @@
+export const LANTERN: { lat: number; lng: number; name: string }[] = [
+  { lat: -57.302789656350086, lng: -63.85253906250001, name: 'Cimetiere' },
+  { lat: -67.62595438857817, lng: -61.87500000000001, name: 'Jetée' },
+  {
+    lat: -75.63953566438667,
+    lng: 27.465820312500004,
+    name: 'Murrieta Oil Fields',
+  },
+  { lat: 74.78737860165964, lng: -22.32421875, name: 'MTP' },
+  { lat: -73.21401304858362, lng: -45.3076171875, name: 'Bay City' },
+];
